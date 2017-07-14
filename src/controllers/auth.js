df88e2ccf2ac9ccb0ee2ec90d8a6968afcd0f973
Michael Pacheco <mpacheco@thelotent.com>
2017-07-13 17:55:08 -0700

    server stuff

---
+++ src/controllers/auth.js
@@ -0,0 +1,14 @@
+const { User } = require('./models');
+const { requireSignIn } = require('../services/passport');
+const getTokenForUser = require('../services/token');
+
+const signIn = (req, res) => {
+  res.send({
+    token: getTokenForUser(req.user)
+  });
+};
+
+module.exports = (app) => {
+  app.post('/signup', signUp);
+  app.post('/signin', requireSignIn, signIn);
+};
---
+++ src/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('brcypt-nodejs');
+
+const UserSchema = mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    validate : {
+      validator: (email) => {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.pre('save', function(next) {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrpyt.hash(this.password, salt, null, (err,hash) => {
+      if(err) return next(err);
+      this.password = hash;
+      next();
+    })
+  });
+});
+
+UserSchema.methods.checkPassword = function(potentialPassword, cb) {
+  bcrypt.compare();
+};
+
+module.exports = UserSchema;
